b1d71f38c091ef718b7389ee7015266d1775a41c
Dillon Reed <c0038731@newcastle.ac.uk>
2026-02-12 13:29:34 +0000

    Updated padding values for WellnessScreen.kt, implemented button to increase glasses state variable

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -1,14 +1,23 @@
 package com.example.basicstatecodelab
 
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
-
-    Text(text = "You've had $count glasses", modifier = modifier.padding(16.dp))
+    var count by remember { mutableStateOf(0) }
+    Column(modifier = modifier) {
+        Text(text = "You've had $count glasses")
+        Button(
+            onClick = { count++ },
+            modifier = Modifier
+        ) { Text(text = "Add glass") }
+    }
 }
--- app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
@@ -1,9 +1,11 @@
 package com.example.basicstatecodelab
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
+    WaterCounter(modifier.padding(16.dp))
 }
